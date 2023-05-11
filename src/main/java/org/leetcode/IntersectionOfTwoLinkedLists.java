231c51b95838aa969e51315842a8461b7c85a79d
최병민 <89714987+men16922@users.noreply.github.com>
2023-05-12 01:42:42 +0900

    LeetCode Top100 Easy Problem

---
+++ src/main/java/org/leetcode/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,42 @@
+package org.leetcode;
+
+/**
+ * packageName    : org.leetcode
+ * fileName       : IntersectionOfTwoLinkedLists
+ * author         : men16
+ * date           : 2023-05-11
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-05-11        men16       최초 생성
+ */
+public class IntersectionOfTwoLinkedLists {
+
+    static class ListNode {
+        int val;
+        MergeTwoSortedLists.ListNode next;
+
+        ListNode() {
+
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, MergeTwoSortedLists.ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
